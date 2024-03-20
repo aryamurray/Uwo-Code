19dce17e2e20e4cd5ffcf232242b8426b05c7dc7
root <root@Thunkstation>
2024-03-20 19:46:54 -0400

    a4 3305

--- 3305/assignment4/assignment-4.c
+++ 3305/assignment4/assignment-4.c
@@ -1,93 +1,229 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <limits.h>
+#include <stdbool.h>
 
-typedef struct Process {
-    int pid;  
+#define MAX_PROCESSES 20
+
+typedef struct
+{
+    int process_id;
     int arrival_time;
     int burst_time;
-    int wait_time; // Added
-    int turnaround_time; // Added
+    int wait_time;
+    int turnaround_time;
 } Process;
 
-void first_come_first_served(Process *processes, int count) {
-    // ... implementation
-}
-
-void shortest_job_first_preemptive(Process *processes, int count) {
-    // ... implementation
-}
-
-void round_robin(Process *processes, int count, int time_quantum) {
-    // ... implementation
-}
-
-Process *read_csv_file(char *filename,int* num_processes) {
+int read_processes(const char *filename, Process processes[])
+{
     FILE *fp = fopen(filename, "r");
-    if (fp == NULL) {
-        printf("Error opening file.\n");
-        return NULL;
+    if (fp == NULL)
+    {
+        return -1; // Error opening file
     }
 
-    // Rough estimate for how many processes you might have
-    Process *processes = malloc(500 * sizeof(Process)); 
+    int num_processes = 0;
+    int id;
+    int burstTime;
 
-    char line[100];
-    while (fgets(line, sizeof(line), fp) != NULL) {
-        char *pid_str = strtok(line, ",");
-        char *burst_str = strtok(NULL, ",");
-
-        if (pid_str && burst_str) {
-            processes[*num_processes].pid = atoi(pid_str + 1); // Skip 'P'
-            processes[*num_processes].arrival_time = 0; // Assume all arrive at 0 
-            processes[*num_processes].burst_time = atoi(burst_str);
-            *num_processes++;
-        }
+    while (fscanf(fp, "P%d,%d\n", &id, &burstTime) == 2)
+    {
+        processes[num_processes].process_id = id;
+        processes[num_processes].burst_time = burstTime;
+        processes[num_processes].wait_time = 0;
+        processes[num_processes].turnaround_time = 0;
+        processes[num_processes].arrival_time = id;
+        num_processes++;
     }
 
     fclose(fp);
-    return processes;
+    return num_processes;
+}
+
+void step(Process Processes[], int num_processes, int pid)
+{
+    for (int i = 0; i < num_processes; i++)
+    {
+        if (i != pid && Processes[i].burst_time != 0)
+        {
+            Processes[i].wait_time++;
+        }
+        if (i == pid)
+        {
+            Processes[i].burst_time--;
+        }
+        if (Processes[i].burst_time != 0)
+        {
+            Processes[i].turnaround_time++;
+        }
+    }
+}
+
+void calculateStats(Process Processes[], int num_processes)
+{
+    int totalWaitTime = 0;
+    int totalTurnAroundTime = 0;
+
+    for (int i = 0; i < num_processes; i++)
+    {
+        totalWaitTime += Processes[i].wait_time;
+        totalTurnAroundTime += Processes[i].turnaround_time;
+    }
+
+    printf("     Average waiting time: %.1f\n", (float)totalWaitTime / num_processes);
+    printf("     Average Turnaround time: %.1f\n\n", (float)totalTurnAroundTime / num_processes);
+}
+
+// void calculateIndividualStats(Process processes, int index){
+
+//     printf("     Average waiting time: %i\n", totalWaitTime/num_processes);
+//     printf("     Average Turnaround time: %i\n\n", totalTurnAroundTime/num_processes);
+// }
+
+void simulate_fcfs(Process processes[], int num_processes)
+{
+    int current_time = 0;
+
+    printf("First Come First Served\n");
+
+    for (int i = 0; i < num_processes; i++)
+    {
+        processes[i].turnaround_time -= processes[i].arrival_time;
+        processes[i].wait_time -= processes[i].arrival_time;
+
+        while (processes[i].burst_time > 0)
+        {
+            printf("T%d : P%d - Burst left %d, Wait Time %d, Turnaround Time %d\n",
+                   current_time, processes[i].process_id, processes[i].burst_time,
+                   processes[i].wait_time, processes[i].turnaround_time);
+            step(processes, 20, i);
+            current_time++;
+        }
+    }
+
+    calculateStats(processes, num_processes);
+}
+
+void simulate_sjf(Process processes[], int num_processes)
+{
+    int current_time = 0;
+
+    printf("Shortest Job First\n");
+
+    while (1)
+    {
+        int shortest_job_index = -1;
+        int shortest_burst_time = INT_MAX;
+
+        // Find the process with the shortest burst time
+        for (int i = 0; i < num_processes; i++)
+        {
+            if (processes[i].arrival_time <= current_time && processes[i].burst_time < shortest_burst_time && processes[i].burst_time > 0)
+            {
+                shortest_burst_time = processes[i].burst_time;
+                shortest_job_index = i;
+            }
+        }
+
+        // If no process is found, all processes have completed
+        if (shortest_job_index == -1)
+        {
+            break;
+        }
+
+        processes[shortest_job_index].turnaround_time -= processes[shortest_job_index].arrival_time;
+        processes[shortest_job_index].wait_time -= processes[shortest_job_index].arrival_time;
+
+        // Run the shortest job
+        while (processes[shortest_job_index].burst_time > 0)
+        {
+            printf("T%d : P%d - Burst left %d, Wait Time %d, Turnaround Time %d\n",
+                   current_time, processes[shortest_job_index].process_id, processes[shortest_job_index].burst_time,
+                   processes[shortest_job_index].wait_time, processes[shortest_job_index].turnaround_time);
+            step(processes, num_processes, shortest_job_index);
+            current_time++;
+        }
+    }
+
+    calculateStats(processes, num_processes);
+}
+
+void simulate_rr(Process processes[], int num_processes, int time_quantum)
+{
+    int current_time = 0;
+
+    printf("Round Robin\n");
+
+    int num_finished = 0;
+
+
+
+        for (int i = 0; i < num_processes+1; i++){
+            if (processes[i].burst_time > 0){
+                for (int j = 0; j < time_quantum && processes[i].burst_time > 0; j++){
+                    printf("T%d : P%d - Burst left %d, Wait Time %d, Turnaround Time %d\n",
+                       current_time, processes[i].process_id, processes[i].burst_time,
+                       processes[i].wait_time, processes[i].turnaround_time);
+                    step(processes, num_processes, i);
+                    current_time++;
+                    if(processes[i].burst_time == 0){
+                        num_finished++;
+                        break;
+                    }
+                }
+                if (num_finished == num_processes) break;
+            }
+            if (i == num_processes) i = 0;
+        }
+
+    calculateStats(processes, num_processes);
 }
 
 
-
-int main(int argc, char *argv[]) {
-    if (argc < 3 || argc > 4) {
-        printf("Usage: %s <algorithm> <filename> [<time_quantum>]\n", argv[0]);
+int main(int argc, char *argv[])
+{
+    if (argc < 3 || argc > 4)
+    {
+        printf("Usage: <-flag> <conditional:time_quantum> <filename> \n\n"); // Add usage details
         return 1;
     }
 
-    char *algorithm = argv[1];
-    char *filename = argv[2];
-    int time_quantum = -1;  // Initialize in case it's not used
+    char algorithm = argv[1][1]; // 'f', 's', or 'r'
+    int time_quantum = 0;
+    char* filename;
 
-    if (strcmp(algorithm, "-r") == 0) {
+    if (algorithm == 'r')
+    {
         if (argc != 4) {
-            printf("Round Robin requires a time quantum.\n");
+            printf("Usage: <-r> <time_quantum> <filename> \n\n"); // Add usage details for round robin
             return 1;
         }
-        time_quantum = atoi(argv[3]);
+        time_quantum = atoi(argv[2]);
+        filename = argv[3];
+    }
+    else
+    {
+        filename = argv[2];
     }
 
-    // Read processes from the CSV file
-    int* num_processes = 0;
-    Process *processes = read_csv_file(filename, num_processes); 
+    Process processes[MAX_PROCESSES];
+    int num_processes = read_processes(filename, processes);
 
-
-    // Select appropriate scheduling algorithm
-    if (strcmp(algorithm, "-f") == 0) {
-        first_come_first_served(processes, num_processes);
-    } else if (strcmp(algorithm, "-s") == 0) {
-        shortest_job_first_preemptive(processes, num_processes);
-    } else if (strcmp(algorithm, "-r") == 0) {
-        round_robin(processes, num_processes, time_quantum);
-    } else {
-        printf("Invalid algorithm.\n");
+    // Select simulation based on algorithm
+    switch (algorithm)
+    {
+    case 'f':
+        simulate_fcfs(processes, num_processes);
+        break;
+    case 's':
+        simulate_sjf(processes, num_processes);
+        break;
+    case 'r':
+        simulate_rr(processes, num_processes, time_quantum);
+        break;
+    default:
+        printf("Invalid algorithm\n");
         return 1;
     }
-
-    calculate_statistics(processes, num_processes);
-
-    free(processes);  // Deallocate memory
-    return 0;
 }
